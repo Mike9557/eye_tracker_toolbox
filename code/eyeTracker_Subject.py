698a4c777bfa2c2a902645ca0e88f76aa325b440
Jiarui Yang <jayng536@uwo.ca>
2019-11-04 22:27:40 -0500

    figure out read_execl

--- code/eyeTracker_Subject.py
+++ code/eyeTracker_Subject.py
@@ -5,9 +5,15 @@ class eyeTracker_Subject:
         self.__inputfile = filename;
         self.__readfile()
     def __readfile(self):
-        dfs = pd.read_excel(self.__inputfile, sheet_name=None)
-        #print(dfs)
+        dfs = pd.read_excel(self.__inputfile, sheet_name="TETSimpleGaze_Order1-999-1")
+        #print(type(dfs))
+        x = 0
+        #print(dfs.columns)
+        #print(dfs['Subject'])
+        #for i in dfs.index:
+            #print(dfs['Subject'][i])
 
-        for key, value in dfs.items():
-            print(key)
-
+        for i in dfs.index:
+            for j in dfs.columns:
+                print(j,i)
+                print(dfs[j][i])
